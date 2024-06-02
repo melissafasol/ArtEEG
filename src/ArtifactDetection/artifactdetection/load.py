d78265f07433afe26cc62d1828d3568c24ab88b5
melissafasol <melissaclairefasol@gmail.com>
2024-06-02 16:28:46 +0100

    add docstrings for all functions

--- src/ArtifactDetection/artifactdetection/load.py
+++ src/ArtifactDetection/artifactdetection/load.py
@@ -5,6 +5,20 @@ import pandas as pd
 
 class LoadFiles():
     
+    """
+    Class to load all recording files and corresponding brain state files (REM, NREM, wake).
+
+    This class handles the loading of recording files in .npy format and brain state files in .pkl format.
+    
+    Attributes:
+    directory_path (str): The path to the directory containing the recording and brain state files.
+    animal_id (str): The identifier for the animal whose files are being loaded.
+
+    Methods:
+    load_two_analysis_files(start_times_dict, end_times_dict): Loads two sets of analysis files based on provided time dictionaries.
+    load_one_analysis_file(start_times_dict, end_times_dict): Loads one set of analysis files based on provided time dictionaries.
+    """
+    
     def __init__(self, directory_path, animal_id):
         self.directory_path = directory_path
         self.animal_id = animal_id
@@ -55,4 +69,61 @@ class LoadFiles():
         br_indices = br_state_file.loc[br_state_file['brainstate'] == br_number].index.to_list()
         br_epochs = np.array(split_epochs)[br_indices]
         return br_epochs
-        
+    
+
+class PreprocessingDat():
+    '''Class to convert EEG recordings into appropriate format. 
+    This analysis pipeline requires that files are in .npy format,
+    if your files are in .dat - apply the functions from this class
+    to convert them to .npy
+    
+    Parameters:
+    downsampling = 1 (if no downsampling)
+    montage_name = montage file (should be in .elc)
+    number of electrodes = 16 
+    
+    '''
+    
+    def __init__(self, downsampling, montage_name, num_electrodes):
+        self.downsampling = downsampling
+        self.montage_name = montage_name
+        self.num_electrodes = num_electrodes
+    
+    '''load a .dat file by interpreting it as int16 and then de-interlacing the 16 channels'''
+    def parse_dat(self, filename, sample_rate):
+        sample_datatype = 'int16'
+        display_decimation = 1
+
+        #load the raw (1-D) data
+        dat_raw = np.fromfile(filename, dtype = sample_datatype)
+
+        #reshape the (2-D) per channel data
+        step = self.num_electrodes * display_decimation
+        dat_chans = [dat_raw[c::step] for c in range(self.num_electrodes)]
+
+        #build the time array 
+        t = np.arange(len(dat_chans[0]), dtype = float) / sample_rate
+
+        return dat_chans, t
+
+    def convert_dat_to_npy(self, filename, path_to_folder, path_to_save_folder, sample_rate, save_as_name):
+
+        os.chdir(path_to_folder)
+        dat_chans, t = self.parse_dat(filename, self.num_electrodes, sample_rate)
+        data_to_save = np.array(dat_chans)
+
+        os.chdir(path_to_save_folder)
+        np.save(save_as_name, data_to_save)
+
+        print('data saved for ' + save_as_name)
+
+
+##example of how to implement functions
+#animal_ID_list = ['424', '430', '433']
+
+#for animal in animal_ID_list:
+#    print(dat_recording[0])
+#    dat_recording = [GRIN2B_file for GRIN2B_file  in os.listdir(path_to_folder) if GRIN2B_file.endswith(animal + '.dat')]
+#    convert_dat_to_npy(filename = dat_recording[0], path_to_folder= path_to_folder, path_to_save_folder=path_to_save_folder, sample_rate=1000,
+#    number_electrodes=16, save_as_name = animal + '_GRIN2B')
+    
