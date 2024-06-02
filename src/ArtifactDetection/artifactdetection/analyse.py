5a8ea4c5fb137b682f336bd63e59fed7a07b0c0c
melissafasol <melissaclairefasol@gmail.com>
2024-06-02 10:39:01 +0100

    class to prepare and plot data

--- src/ArtifactDetection/artifactdetection/analyse.py
+++ src/ArtifactDetection/artifactdetection/analyse.py
@@ -1,63 +1,75 @@
-import os
+import os 
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-class AnalyseSlope():
-    '''Class to analyse results from explore'''
-    def __init__(self, power_ls, slope_int_ls):
-        self.power_ls = power_ls
-        self.slope_ints_ls = slope_int_ls
+
+class Analysis:
+    def __init__(self, directory_path, analysis_ls):
+        self.directory_path = directory_path
+        self.analysis_ls = analysis_ls
+        self.analytics_df_ls = []
+        self.clean_power_df = []
+        self.noise_power_df = []
     
-    def concat_results(self):
-        '''
-        Concatenates results from explore class into dataframes to plot.
-        '''
-        test_plot_df_ls = []
-        slopes_ints_ls = []
-
-        for power_plot_df in self.power_ls:
-            test_plot_df_ls.append(power_plot_df)
-        for slope_int_df in self.slope_ints_ls:
-            slopes_ints_ls.append(slope_int_df)
-        
-        power_concat = pd.concat(test_plot_df_ls)
-        slopes_int_concat = pd.concat(slopes_ints_ls)   
-        
-        return power_concat, slopes_int_concat
+    def read_files(self, animal):
+        power_file = pd.read_csv(f'{self.directory_path}{animal}_power.csv')
+        slope_file = pd.read_csv(f'{self.directory_path}{animal}_slope.csv')
+        return power_file, slope_file
     
-    def identify_noise(self, slopes_int_concat, slope_threshold):
-        '''
-        Identifies noisy epochs based on a slope threshold.
-
-        Parameters:
-       
-        - slopes_int_concat (pd.DataFrame): A concatenated dataframe containing slope and intercept data.
-        - slope_threshold (int): An integer value that sets the boundary to mark epochs as noisy or clean.
-
-        Returns:
-        - unq_noise_indices: list with indices of noisy epochs
-        '''
-        noise_indices = slopes_int_concat.loc[slopes_int_concat['Slope'] < slope_threshold]
-        unq_noise_indices = np.unique(noise_indices['Epoch'])
-        return unq_noise_indices
+    def filter_data(self, slope_file, power_file, slope_threshold = None):
+        if slope_threshold is None:
+            slope_threshold = -8
+            
+        clean_df = slope_file.loc[~(slope_file['Slope'] < slope_threshold)]
+        clean_indices = np.unique(clean_df['Epoch'])
+        clean_power = power_file.loc[power_file['Epoch'].isin(clean_indices)]
         
+        noisy_df = slope_file.loc[(slope_file['Slope'] < slope_threshold)]
+        noisy_indices = np.unique(noisy_df['Epoch'])
+        noise_power = power_file.loc[power_file['Epoch'].isin(noisy_indices)]
         
-    def prep_df_plot(self, unq_noise_indices, power_concat):
-        '''
-        Parameters:
-         - unq_noise_indices: list with indices of noisy epochs
-         - power_concat (pd.DataFrame): A concatenated dataframe containing power data.
+        return clean_power, noise_power, clean_df, noisy_df
+    
+    def create_analytics_df(self, animal, clean_df, noisy_df):
+        analytics_df = pd.DataFrame(data={
+            'Animal_ID': [animal], 
+            'Noisy_Epochs': len(noisy_df['Epoch']),
+            'Clean_Epochs': len(clean_df['Epoch'])
+        })
+        return analytics_df
+    
+    def analyze(self):
+        for animal in self.analysis_ls:
+            power_file, slope_file = self.read_files(animal)
+            clean_power, noise_power, clean_df, noisy_df = self.filter_data(slope_file, power_file)
+            
+            self.clean_power_df.append(clean_power)
+            self.noise_power_df.append(noise_power)
+            
+            analytics_df = self.create_analytics_df(animal, clean_df, noisy_df)
+            self.analytics_df_ls.append(analytics_df)
         
-        Returns:
-        - all_epochs (pd.DataFrame): A dataframe with all epochs 
-        - clean_epochs (pd.DataFrame): A dataframe containing only the clean epochs.
-        - noisy_epochs (pd.DataFrame): A dataframe containing only the noisy epochs.
-        '''
-        clean_df = power_concat[~power_concat['Epoch'].isin(unq_noise_indices)]
-        noise_df = power_concat[power_concat['Epoch'].isin(unq_noise_indices)]
+        self.analytics_df_concat = pd.concat(self.analytics_df_ls)
+        self.power_concat = pd.concat(self.clean_power_df)
+    
+    def plot_results(self, palette=None):
+        if palette is None:
+            palette = ['orange', 'orangered','lightsalmon','darkred', 'gold','yellowgreen','darkseagreen',
+                       'darkolivegreen', 'teal', 'skyblue', 'darkblue','black', 'slategrey',
+                       'palevioletred','plum', 'deeppink', 'mediumpurple', 'peru']
         
-        all_epochs = power_concat.groupby('Frequency')['Power'].mean().reset_index()
-        noisy_epochs = noise_df.groupby('Frequency')['Power'].mean().reset_index()
-        clean_epochs = clean_df.groupby('Frequency')['Power'].mean().reset_index()
+        sns.set_style("white")
+        fig, axs = plt.subplots(1, 1, figsize=(10,8), sharex=True, sharey=True)
+        sns.lineplot(data=self.power_concat, x='Frequency', y='Power', hue='Animal_ID', 
+                     errorbar=('se'), linewidth=2, palette=palette)
         
-        return all_epochs, noisy_epochs, clean_epochs
+        sns.despine()
+        axs.set_yscale('log')
+        axs.set_xlim(1, 48)
+        axs.set_ylim(10**-2, 10**4)
+        axs.set_xlabel("Frequency (Hz)")
+        axs.set_ylabel(r"log Power ($\mu V^2$)")
+        fig.suptitle('After Artifact Removal', y=0.96, fontsize=15, fontweight='bold')
+        plt.show()
